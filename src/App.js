b47a413028180b9eaf312265df7d199ab8546c7f
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-03 13:14:28 +0200

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./Form";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <div className="Footer">
+      Coded by
+      <a target="_blank" rel="noreferrer" href="https://github.com/MateaZerjav">
+        Matea Zerjav
+      </a>
+      , is
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/MateaZerjav/react-weather-app"
+      >
+        open-sourced on GitHub
+      </a>
+      and hosted
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://master--stellular-youtiao-fb0c4c.netlify.app/"
+      >
+        Netlify.
+      </a>
+    </div>
+  );
+}
